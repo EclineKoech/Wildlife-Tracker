42cba937fce84cc8da5d9ae1c41eb9c6855021ce
Ecline Koech <eclinejerutokoech@gmail.com>
2021-08-25 23:12:05 +0300

    change form layout

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,33 @@
 package models;
 
 public class Animal {
+  private int id;
+  private  String name;
+
+  public Animal(String name, int id) {
+    this.name = name;
+    this.id = id;
+  }
+
+  public Animal(String name, int age, String location, String health) {
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Animal() {
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Integer getId() {
+    return id;
+  }
 }
