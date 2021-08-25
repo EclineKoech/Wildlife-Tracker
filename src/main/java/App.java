48e1ed7701f9c50741a7e38dba63dc810d6bcecf
Ecline Koech <eclinejerutokoech@gmail.com>
2021-08-25 23:10:40 +0300

    add get path

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,45 @@
+import models.Animal;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.route.HttpMethod.post;
+
 public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      return new ModelAndView(model, "layout.hbs");
+    }, new HandlebarsTemplateEngine());
+
+    get("/home.hbs", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      return new ModelAndView(model, "home.hbs");
+    }, new HandlebarsTemplateEngine());
+
+    get("/animal.hbs", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      return new ModelAndView(model, "animal.hbs");
+    }, new HandlebarsTemplateEngine());
+
+
+    get("/sightings.hbs", (request, response) -> {
+      return new ModelAndView(new HashMap(), "sightings.hbs");
+    }, new HandlebarsTemplateEngine());
+    post("/sightings", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      int age = Integer.parseInt(request.queryParams("age"));
+      String location = request.queryParams("health");
+      String health = request.queryParams("health");
+      Animal animal = new Animal(name, age, location,health);
+      model.put("animal", animal);
+      return new ModelAndView(model, "sightings.hbs");
+    }, new HandlebarsTemplateEngine());
+  }
 }
+
