cc4b57f0067eeeb6d6f36f987d4f6ec4e9b5396d
Ecline Koech <eclinejerutokoech@gmail.com>
2021-08-26 04:16:01 +0300

    create db class

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -1,4 +1,19 @@
 package models;
 
+import java.sql.Connection;
+
 public class DB {
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker");
+
+  private static class Sql2o {
+    public Sql2o(String s, Object o, Object o1) {
+    }
+
+    public Sql2o(String s) {
+    }
+
+    public Connection open() {
+      return null;
+    }
+  }
 }
