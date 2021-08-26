726c5d531617862aa564e47850a834f123fe28d4
Ecline Koech <eclinejerutokoech@gmail.com>
2021-08-26 04:14:32 +0300

    instantiate endangered animal with animal id

--- src/main/java/models/EndangeredAnimal.java
+++ src/main/java/models/EndangeredAnimal.java
@@ -1,4 +1,22 @@
 package models;
 
 public class EndangeredAnimal {
+  private String name;
+  private int id;
+
+  public EndangeredAnimal(String name, int id, String health, int age){
+    this.name=name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
 }
