17d829b3f1f0ad66076bb4ad37487a5ee093cf04
Ecline Koech <eclinejerutokoech@gmail.com>
2021-08-27 19:01:03 +0300

    change path of form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,23 +1,21 @@
 import models.Animal;
+import models.EndangeredAnimal;
 import models.Sighting;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.sql.Timestamp;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
 
 public class App {
-  private static Object sighting;
 
   public static void main(String[] args) {
     staticFileLocation("/public");
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      return new ModelAndView(model, "layout.hbs");
+      return new ModelAndView(model, "home.hbs");
     }, new HandlebarsTemplateEngine());
 
     get("/home", (request, response) -> {
@@ -25,41 +23,27 @@ public class App {
       return new ModelAndView(model, "home.hbs");
     }, new HandlebarsTemplateEngine());
 
-    get("/animal.hbs", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      return new ModelAndView(model, "animal.hbs");
-    }, new HandlebarsTemplateEngine());
-
-
-    get("/sighting", (request, response) -> {
-      return new ModelAndView(new HashMap(), "sighting.hbs");
-    }, new HandlebarsTemplateEngine());
-
     get("/form", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      return  new ModelAndView(model,"form.hbs");
-    },new HandlebarsTemplateEngine());
-    post("/form", (request, response) -> {
+      return new ModelAndView(model, "form.hbs");
+    }, new HandlebarsTemplateEngine());
+
+    get("/sighting", (request, response) -> {
+      Map<String,Object> model = new HashMap<>();
+      model.put("sighting", Sighting.getAll());
+      model.put("animal", EndangeredAnimal.all());
+      return new ModelAndView(model, "sighting.hbs");
+    }, new HandlebarsTemplateEngine());
+    post("/sighting", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       String rangerName = request.queryParams("ranger_name");
       String animalName = request.queryParams("animal_name");
       int age = Integer.parseInt(request.queryParams("age"));
       String location = request.queryParams("location");
       String health = request.queryParams("health");
-      Animal animal = new Animal(rangerName, animalName, age, location,health);
+      Animal animal = new Animal(rangerName, animalName, age, location, health);
       model.put("animal", animal);
       return new ModelAndView(model, "sighting.hbs");
-    }, new HandlebarsTemplateEngine());
-
-    post("/sighting", (req, res) -> {
-      Map<String, Object> model = new HashMap<>();
-      String name = req.queryParams("name");
-      Timestamp timestamp = null;
-      Sighting animal = new Sighting(name, null);
-      animal.save();
-      List<Sighting> siteObj = Sighting.getAll();
-      model.put("sighting", sighting);
-      return new ModelAndView(model,"sighting.hbs");
     }, new HandlebarsTemplateEngine());
   }
 }
